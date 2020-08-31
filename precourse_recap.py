698238248e86d107b9ac8e84a6f2117d1b09054e
Cyril Ishabiyi <cyrilishabiyi@Cyrils-MacBook-Air.local>
2020-08-31 15:22:25 +0100

    first commit

---
+++ precourse_recap.py
@@ -0,0 +1,10 @@
+# Simple code to understand what classes a person takes
+
+module = input("What module are you taking? ")
+
+if module == "software development":
+    print("Yaay you are studying in person!!!")
+elif module == "data analysis":
+    print("Yaay you are studying virtually!!!")
+else:
+    print("Awwn you arent studying :(")
